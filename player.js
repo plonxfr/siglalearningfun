138329b18af80ac12594f98c23e4df94a77cb0d9
plonxfr <luna0lunar@hotmail.com>
2026-03-17 01:25:00 +0800

    Update player.js with player object code

--- player.js
+++ player.js
@@ -1,2 +1,17 @@
-// player.js content goes here
-console.log('Player file loaded.');
+const player = {
+    name: 'PlayerName',
+    score: 0,
+    level: 1,
+    reset: function() {
+        this.score = 0;
+        this.level = 1;
+    },
+    levelUp: function() {
+        this.level += 1;
+    },
+    addScore: function(points) {
+        this.score += points;
+    }
+};
+
+export default player;
